57e8a3f4d611a5b09d3be8f71ffd1232a87e0606
Max <7087907+maxpromer@users.noreply.github.com>
2022-06-27 21:55:15 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP32 chip.
+
+  Note: This requires ESP32 support package:
+    https://github.com/espressif/arduino-esp32
+
+  Please be sure to select the right ESP32 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+#define BLYNK_TEMPLATE_ID "TMPLwbjLQnOX"
+#define BLYNK_DEVICE_NAME "TestKidBright"
+#define BLYNK_AUTH_TOKEN "rmw01sdzlx8CgkS2pVu3KF903sUzqgNm"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <Wire.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "@lamloeiWLAN";
+char pass[] = "@lamloeiWLAN";
+
+#define LM73_ADDR 0x4D
+// Cr. http://quantum.engr.tu.ac.th/smf/index.php?topic=94.0
+float readTemperature() {
+  Wire1.beginTransmission(LM73_ADDR);
+  Wire1.write(0x00); // Temperature Data Register
+  Wire1.endTransmission();
+ 
+  uint8_t count = Wire1.requestFrom(LM73_ADDR, 2);
+  float temp = 0.0;
+  if (count == 2) {
+    byte buff[2];
+    buff[0] = Wire1.read();
+    buff[1] = Wire1.read();
+    temp += (int)(buff[0]<<1);
+    if (buff[1]&0b10000000) temp += 1.0;
+    if (buff[1]&0b01000000) temp += 0.5;
+    if (buff[1]&0b00100000) temp += 0.25;
+    if (buff[0]&0b10000000) temp *= -1.0;
+  }
+  return temp;
+}
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  float t = readTemperature();
+  Blynk.virtualWrite(V0, t);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  Wire1.begin(4, 5);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
+
